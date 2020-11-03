055ce9b7382c5cbdb1dc2d9f588fa4ddef365ddd
jonreesecooper <70401154+jonreesecooper@users.noreply.github.com>
2020-11-03 18:24:58 -0500

    Add files via upload

---
+++ StringFunctions/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first = "Jon is really ";
+            string second = "good at ";
+            string third = "concatenating strings!";
+            string concat = first + second + third;
+            Console.WriteLine(concat);
+            string upper = concat.ToUpper();
+            Console.WriteLine(upper);
+            StringBuilder sb = new StringBuilder();
+            sb.Append("This is my first sentence.  ");
+            sb.Append("This is my second sentence.  ");
+            sb.Append("This right here, well this is my third sentence.  ");
+            sb.Append("Wow, what an amazing paragraph!");
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
