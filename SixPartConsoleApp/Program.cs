44755eac3c83b1023b265b0b0f065ee40a60c8f9
jonreesecooper <70401154+jonreesecooper@users.noreply.github.com>
2020-11-04 15:23:01 -0500

    Add files via upload

--- SixPartConsoleApp/Program.cs
+++ SixPartConsoleApp/Program.cs
@@ -11,7 +11,6 @@ namespace SixPartConsoleApp
         static void Main(string[] args)
         {
             string[] strArray = new string[] { "Jon ", "Adam ", "Charley ", "Ryan ", "Dave ", "Omar " };
-            string[] emptyArray = new string[6];
             Console.WriteLine("Please provide some text to add to string.");
             string addString = Console.ReadLine();
             for (int i = 0; i < strArray.Length; i++)
@@ -22,28 +21,29 @@ namespace SixPartConsoleApp
             {
                 Console.WriteLine(newString);
             }
-           
 
+
+            string[] newArray = new string[] { "Hello, ", "this", "is", "my", "array!" };
             bool whileBool = false;
             while (!whileBool)
             {
-                foreach (string newString in strArray)
+                foreach (string item in newArray)
                 {
-                    Console.WriteLine(newString);
+                    Console.WriteLine(item);
                 }
                 // the following fixes the infinite while loop
                 whileBool = true;
             }
 
             int lessThan = 10;
-            while (lessThan < 20)
+            while (lessThan < 14)
             {
                 Console.WriteLine(lessThan);
                 lessThan++;
             }
 
             int orEqual = 15;
-            while (orEqual <= 25)
+            while (orEqual <= 19)
             {
                 Console.WriteLine(orEqual);
                 orEqual++;
@@ -52,16 +52,42 @@ namespace SixPartConsoleApp
             List<string> lastNames = new List<string>() { "Cooper", "Gouldner", "Johnson", "Zuniga", "Gailey", "Olmedo" };
             Console.WriteLine("Please enter a last name to search.");
             string nameSearch = Console.ReadLine();
-            foreach (string name in lastNames)
+            bool newBool = false;
+            while (!newBool)
             {
-                if(name == nameSearch)
+                if (lastNames.Contains(nameSearch) == true)
                 {
-                    Console.WriteLine(name);
+                    Console.WriteLine("A match was found at index: " + lastNames.IndexOf(nameSearch));
+                    newBool = true;
                 }
                 else
                 {
-                    Console.Write("Oops, we have no last name meeting your search criteria!");
+                    Console.WriteLine("No matches found for that search.");
+                    Console.WriteLine("Please enter a last name to search.");
+                    nameSearch = Console.ReadLine();
                 }
+            }
+
+            List<string> videoGames = new List<string>() { "Borderlands", "Fallout 4", "Final Fantasy XV", "Borderlands", "Dishonored" };
+            Console.WriteLine("Please enter a video game to search.");
+            string gameSearch = Console.ReadLine();
+            for (int i = 0; i < videoGames.Count; i++)
+            {
+                bool thirdBool = false;
+                while(!thirdBool)
+                {
+                    if (videoGames[i] == gameSearch)
+                    {
+                        Console.WriteLine("A match was found at index: " + videoGames.IndexOf(videoGames[i]));
+                        thirdBool = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Oops, no entries found!");
+                        thirdBool = true;
+                    }
+                }
+
             }
 
             Console.ReadLine();
