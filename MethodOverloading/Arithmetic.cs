c39dd3d2271314e10b7a237aca035acb92087323
jonreesecooper <70401154+jonreesecooper@users.noreply.github.com>
2020-11-05 16:10:06 -0500

    Add files via upload

---
+++ MethodOverloading/Arithmetic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    public class Arithmetic
+    {
+        public int AddingTo(int x)
+        {
+            return x + 25;
+        }
+
+        public int AddingTo(decimal x)
+        {
+            return Convert.ToInt32(x + 50);
+        }
+
+        public string AddingTo(string x)
+        {
+            Convert.ToInt32(x);
+            return x + 75;
+        }
+    }
+}
---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arithmetic arithmetic = new Arithmetic();
+            int intArithmetic = arithmetic.AddingTo(15);
+            Console.WriteLine(intArithmetic);
+            int decArithmetic = arithmetic.AddingTo(20.0m);
+            Console.WriteLine(decArithmetic);
+            string strArithmetic = arithmetic.AddingTo("55");
+            Console.WriteLine(strArithmetic);
+            Console.ReadLine();
+        }
+    }
+}
