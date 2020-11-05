3b98f1315425bbe9026a3b4818299a14f1b331c0
jonreesecooper <70401154+jonreesecooper@users.noreply.github.com>
2020-11-05 14:24:13 -0500

    Add files via upload

---
+++ MethodsApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a whole number.");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            int firstAnswer = AddingTo(userInput);
+            Console.WriteLine(firstAnswer);
+            Console.ReadLine();
+        }
+    }
+}
