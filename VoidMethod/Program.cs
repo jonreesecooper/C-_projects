515ae8c357ddbe3e91deb0f107e12f0c35c89f04
jonreesecooper <70401154+jonreesecooper@users.noreply.github.com>
2020-11-05 18:06:28 -0500

    Add files via upload

---
+++ VoidMethod/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VoidMath voidmath = new VoidMath();
+            voidmath.DoMath(5, 2);
+            voidmath.DoMath(input1: 6, input2: 7);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ VoidMethod/VoidMath.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    public class VoidMath
+    {
+        public void DoMath(int input1, int input2)
+        {
+            int voided = input1 * 5;
+            int displayed = input2;
+            Console.WriteLine("The second parameter of this method is {0}", displayed);
+        }
+    }
+}
