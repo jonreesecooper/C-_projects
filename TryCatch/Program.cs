e6b775af7ed9a9add3f9a0e1fdf0051f58c8c5dc
jonreesecooper <70401154+jonreesecooper@users.noreply.github.com>
2020-11-05 10:52:34 -0500

    Add files via upload

---
+++ TryCatch/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intList = new List<int>() { 8, 6, 7, 5, 3, 0, 9 };
+            Console.WriteLine("Please enter a number:");
+            try
+            {
+                foreach(int number in intList)
+                {
+                    int answer = Convert.ToInt32(Console.ReadLine());
+                    int result = number / answer;
+                    Console.WriteLine(number + " divided by " + answer + " equals " + result);
+                }
+
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(DivideByZeroException ex)
+            {
+               Console.WriteLine(ex.Message);
+            }
+            catch(Exception ex)
+            {
+               Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Operation completed, moving on with program.");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
