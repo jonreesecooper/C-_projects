278e8fcfa32de0a9de1666fddbae0115ae24f0b1
jonreesecooper <70401154+jonreesecooper@users.noreply.github.com>
2020-11-06 16:45:58 -0500

    Add files via upload

--- Employee/Program.cs
+++ Employee/Program.cs
@@ -12,6 +12,9 @@ namespace Employee
         {
             Employee employee = new Employee() { FirstName = "Sample", LastName = "Student", Id = 1 };
             Employee employee1 = new Employee() { FirstName = "Jon", LastName = "Cooper", Id = 2 };
+            IQuittable iquittable = new Employee();
+            employee.SayName();
+            iquittable.Quit();
             if(employee == employee1)
             {
                 Console.WriteLine("It is a match!");
