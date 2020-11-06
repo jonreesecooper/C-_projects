39b8692e16922271e2076d843eb75519a04b9441
jonreesecooper <70401154+jonreesecooper@users.noreply.github.com>
2020-11-06 17:36:52 -0500

    Add files via upload

--- Employee/Employee.cs
+++ Employee/Employee.cs
@@ -6,19 +6,21 @@ using System.Threading.Tasks;
 
 namespace Employee
 {
-    public class Employee: Person, IQuittable
+    public class Employee<T>: Person, IQuittable
     {
+        public List<T> Things { get; set; }
         public void Quit()
-    {
+        {
         Console.WriteLine("I quit!");
-    }
-        public static bool operator==(Employee person, Person person1)
-        {
-            return (person.Id == person1.Id);
         }
-        public static bool operator!=(Employee person, Person person1)
-        {
-            return (person.Id != person.Id);
-        }
+        //public static bool operator==(Employee person, Person person1)
+        //{
+        //    return (person.Id == person1.Id);
+        //}
+        //public static bool operator!=(Employee person, Person person1)
+        //{
+        //    return (person.Id != person.Id);
+        //}
+
     }
 }
--- Employee/Program.cs
+++ Employee/Program.cs
@@ -10,18 +10,30 @@ namespace Employee
     {
         static void Main(string[] args)
         {
-            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student", Id = 1 };
-            Employee employee1 = new Employee() { FirstName = "Jon", LastName = "Cooper", Id = 2 };
-            IQuittable iquittable = new Employee();
-            employee.SayName();
-            iquittable.Quit();
-            if(employee == employee1)
+            //Employee employee = new Employee() { FirstName = "Sample", LastName = "Student", Id = 1 };
+            //Employee employee1 = new Employee() { FirstName = "Jon", LastName = "Cooper", Id = 2 };
+            //IQuittable iquittable = new Employee();
+            //employee.SayName();
+            //iquittable.Quit();
+            //if(employee == employee1)
+            //{
+            //    Console.WriteLine("It is a match!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("It is not a match.");
+            //}
+            Employee<string> employee = new Employee<string>();
+            employee.Things = new List<string>() { "This", "is", "my", "list" };
+            Employee<int> employee1 = new Employee<int>();
+            employee1.Things = new List<int>() { 1, 2, 3, 4 };
+            foreach (string things in employee.Things)
             {
-                Console.WriteLine("It is a match!");
+                Console.WriteLine(things);
             }
-            else
+            foreach (int things in employee1.Things)
             {
-                Console.WriteLine("It is not a match.");
+                Console.WriteLine(things);
             }
             Console.ReadLine();
         }
