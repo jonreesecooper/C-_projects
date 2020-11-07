c17b6057c99f7f8bff4665fa0d59cbbc990b095a
jonreesecooper <70401154+jonreesecooper@users.noreply.github.com>
2020-11-07 15:08:43 -0500

    Add files via upload

---
+++ DataType/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 3.15m;
+            Console.WriteLine("The amount is {0}", number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
