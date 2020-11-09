b6a38ce55e23f8e6d4c279c5047014d33b3d15f3
jonreesecooper <70401154+jonreesecooper@users.noreply.github.com>
2020-11-09 18:00:26 -0500

    Add files via upload

--- Birthday/Program.cs
+++ Birthday/Program.cs
@@ -14,16 +14,20 @@ namespace Birthday
             try
             {
                 var userInput = Convert.ToInt32(Console.ReadLine());
+                if (userInput < 1)
+                {
+                    throw new ArgumentException();
+                }
                 var myDate = DateTime.Now.Year;
                 var birthYear = myDate - userInput;
                 Console.WriteLine("You were born in the year {0}", birthYear);
                 Console.ReadLine();
             }
-            //catch when (userInput < 1)
-            //{
-            //    Console.WriteLine("You did not enter a positive whole number");
-            //    Console.ReadLine();
-            //}
+            catch (ArgumentException)
+            {
+                Console.WriteLine("You did not enter a positive whole number");
+                Console.ReadLine();
+            }
             catch (Exception)
             {
                 Console.WriteLine("The age you entered was input incorrectly.");
