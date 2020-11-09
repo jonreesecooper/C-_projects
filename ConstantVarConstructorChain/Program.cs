e8ae5549b796b06dbd0f7b997fe35d8a2ea680f8
jonreesecooper <70401154+jonreesecooper@users.noreply.github.com>
2020-11-09 13:58:58 -0500

    Add files via upload

---
+++ ConstantVarConstructorChain/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstantVarConstructorChain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string mySchool = "The Tech Academy";
+            var myName = "Jon";
+            Student student1 = new Student(myName);
+            Student student2 = new Student("Joe", "Python", mySchool);
+            Console.WriteLine("{0} is studying {1} at {2}.", student1.Name, student1.Course, student1.School);
+            Console.WriteLine("{0} is studying {1} at {2}.", student2.Name, student2.Course, student2.School);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConstantVarConstructorChain/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstantVarConstructorChain
+{
+    class Student
+    {
+        public Student(string name) : this(name, "C#", "The Tech Academy")
+        {
+
+        }
+        public Student(string name, string course, string school)
+        {
+            Name = name;
+            Course = course;
+            School = school;
+        }
+
+        public string Name { get; set; }
+        public string Course { get; set; }
+        public string School { get; set; }
+    }
+}
