00f8410b5b6be52efaac86f26016adcfe64f9a70
jonreesecooper <70401154+jonreesecooper@users.noreply.github.com>
2020-11-09 11:55:58 -0500

    Add files via upload

---
+++ TodaysDate/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodaysDate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime todaysDate = DateTime.Now;
+            Console.WriteLine("Today's date is {0}", todaysDate);
+            Console.WriteLine("Please enter a number:");
+            double userInput = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("In {0} hours it will be {1}", userInput, todaysDate.AddHours(userInput));
+            Console.ReadLine();
+        }
+    }
+}
