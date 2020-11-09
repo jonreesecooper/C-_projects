21db4a9e60c86ccdb58cfe5d765941601eff8a3d
jonreesecooper <70401154+jonreesecooper@users.noreply.github.com>
2020-11-09 17:42:39 -0500

    Add files via upload

---
+++ Birthday/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Birthday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your age:");
+            try
+            {
+                var userInput = Convert.ToInt32(Console.ReadLine());
+                var myDate = DateTime.Now.Year;
+                var birthYear = myDate - userInput;
+                Console.WriteLine("You were born in the year {0}", birthYear);
+                Console.ReadLine();
+            }
+            //catch when (userInput < 1)
+            //{
+            //    Console.WriteLine("You did not enter a positive whole number");
+            //    Console.ReadLine();
+            //}
+            catch (Exception)
+            {
+                Console.WriteLine("The age you entered was input incorrectly.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
