c5480e105494590fef2350a2fb615e1c709bab89
jonreesecooper <70401154+jonreesecooper@users.noreply.github.com>
2020-11-16 12:14:02 -0500

    commit
    commit

---
+++ Challenge/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Challenge.Models;
+
+namespace Challenge.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Click the button to get your current time.";
+            return View();
+        }
+
+        public EventHandler ButtonClicked;
+        protected virtual void OnButtonClicked(EventArgs e)
+        {
+            EventHandler handler = ButtonClicked;
+            var timeLabel = DateTime.Now;
+            handler?.Invoke(this, e);
+        }
+        
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
