5f3c0754782d7baed72433303b5794b21fab1e6f
jonreesecooper <70401154+jonreesecooper@users.noreply.github.com>
2020-11-17 10:13:24 -0500

    commit
    commit

---
+++ CurrentDate/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CurrentDate.Models;
+
+namespace CurrentDate.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+            var model = new GetTheDate();
+            model.SetDate();
+            return View(model);
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CurrentDate/Models/GetTheDate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CurrentDate.Models
+{
+    public class GetTheDate
+    {
+        public DateTime TheDate { get; set; }
+
+        public void SetDate()
+        {
+            TheDate = DateTime.Now;
+            string sendDate = TheDate.ToString();
+        }
+    }
+}
