e16d7178657f65fcc1693686e0237b5a74e45421
jonreesecooper <70401154+jonreesecooper@users.noreply.github.com>
2020-11-17 13:01:38 -0500

    commit
    commit

---
+++ FinalProject/Models/Grade.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalProject.Models
+{
+    public class Grade
+    {
+        public int GradeID { get; set; }
+        public string GradeName { get; set; }
+        public string Section { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+    }
+}
---
+++ FinalProject/Models/SchoolContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace FinalProject.Models
+{
+    public class SchoolContext: DbContext
+    {
+        public SchoolContext(): base()
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
